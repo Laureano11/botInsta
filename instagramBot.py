51b62141678c859d02e13321b7be58bac14a26ef
Laureano11 <lenriquezabala@frba.utn.edu.ar>
2021-11-21 23:53:57 -0300

    bot send msj to all users

---
+++ instagramBot.py
@@ -0,0 +1,9 @@
+from instabot import Bot
+
+miMain= Bot()
+
+
+
+mensajes = miMain.get_messages()
+
+print(mensajes)
--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from instabot import Bot
+import shutil
+import time
+from tqdm import tqdm
 
-botardo= Bot()
+botardo = Bot()
 
-botardo.login(username="")
+botardo.login(username="", password="")
+
+
+
+
+def send_message_all_users(botUse, mensaje):
+    for follower in botUse.followers :
+        botUse.send_message(mensaje, follower)
+    print("Sent An Individual Messages To Your Followers..")
+    time.sleep(1)
+    exit()
+
+
+message = input("Que mensaje desea enviar?")
+
+##send_message_all_users(botardo, message)
+
+print(botardo.followers)
+
+botardo.send_messages(message, botardo.followers)
+
+def all_followers_msg(botToUse):
+    for users in
 
 
